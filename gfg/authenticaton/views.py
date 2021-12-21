0fff3f6acea7a030ff789c11b9fb201037210672
shivareddy chennu <shivareddychennu7373@gmail.com>
2021-12-21 19:11:12 +0530

    first commit

---
+++ gfg/authenticaton/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+# Create your views here.
+def home(request):
+    return render(request, 'authenticaton/index.html')
+def signup(request):
+    if request.method == "POST":
+        # username = request.POST.get('username')
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        
+        myuser.save()
+        messages.success(request, 'Your Account has been Successfully created')
+
+        return redirect('signin')
+
+
+    return render(request, 'authenticaton/signup.html')
+def signin(request):
+    return render(request, 'authenticaton/signin.html')
+def signout(request):
+    return render(request, 'authenticaton/signup.html')
